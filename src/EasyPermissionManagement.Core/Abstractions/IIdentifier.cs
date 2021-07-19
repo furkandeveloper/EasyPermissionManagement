c0b67f1b113ce2620eb87717ef429c8fbccadec3
furkandeveloper <furkan.dvlp@gmail.com>
2021-07-20 01:07:37 +0300

    ⛏ Add IIdentifier interface

---
+++ src/EasyPermissionManagement.Core/Abstractions/IIdentifier.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyPermissionManagement.Core.Abstractions
+{
+    /// <summary>
+    /// This interface includes signature for Identifier objects
+    /// </summary>
+    public interface IIdentifier
+    {
+        /// <summary>
+        /// Get Identifier key
+        /// </summary>
+        /// <param name="provider">
+        /// Identifier provider. For Example: User, organization, team, company, supplier etc.
+        /// </param>
+        /// <returns>
+        /// Task of string
+        /// </returns>
+        Task<string> GetAsync(string provider);
+    }
+}
