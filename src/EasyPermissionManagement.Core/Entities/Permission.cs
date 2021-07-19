d26e1bd57ab218dad98a57a36c9d40b8e07682a1
furkandeveloper <furkan.dvlp@gmail.com>
2021-07-20 01:12:32 +0300

    ⛏ Add Permission table

---
+++ src/EasyPermissionManagement.Core/Entities/Permission.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyPermissionManagement.Core.Entities
+{
+    /// <summary>
+    /// Permission
+    /// </summary>
+    public class Permission : BaseEntity
+    {
+        /// <summary>
+        /// Name of Permission
+        /// </summary>
+        public string Key { get; set; }
+
+
+        /// <summary>
+        /// Provider of Permission
+        /// </summary>
+        public string Provider { get; set; }
+    }
+}
