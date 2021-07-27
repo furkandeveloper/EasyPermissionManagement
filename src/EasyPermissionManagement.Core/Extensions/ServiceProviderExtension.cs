ade20bff9b86b76006e274a0b0e856902cfecfce
furkandeveloper <furkan.dvlp@gmail.com>
2021-07-28 00:09:14 +0300

    ⛏ Add Find Identifier extension for IServiceProvider

---
+++ src/EasyPermissionManagement.Core/Extensions/ServiceProviderExtension.cs
@@ -0,0 +1,59 @@
+﻿using EasyPermissionManagement.Core.Abstractions;
+using EasyPermissionManagement.Core.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyPermissionManagement.Core.Extensions
+{
+    /// <summary>
+    /// Service Provider Extension
+    /// </summary>
+    public static class ServiceProviderExtension
+    {
+        /// <summary>
+        /// Find Identifier by provider
+        /// </summary>
+        /// <param name="serviceProvider">
+        ///  Defines a mechanism for retrieving a service object; that is, an object that provides custom support to other objects.
+        /// </param>
+        /// <param name="provider">
+        /// Identifier provider
+        /// </param>
+        /// <returns>
+        /// IIdentifier object
+        /// </returns>
+        public static IIdentifier FindIdentifier(this IServiceProvider serviceProvider, string provider)
+        {
+            if (string.IsNullOrEmpty(provider))
+            {
+                throw new ArgumentException($"'{nameof(provider)}' cannot be null or empty.", nameof(provider));
+            }
+
+            var type = typeof(IIdentifier);
+
+            var service = AppDomain.CurrentDomain.GetAssemblies().SelectMany(s => s.GetTypes())
+                .Where(p => type.IsAssignableFrom(p) && !p.IsInterface)
+                .Where(c => c.GetCustomAttribute<RegisterIdentifierAttribute>().Provider == provider)
+                .FirstOrDefault();
+
+            if (service is null)
+                throw new NotImplementedException("Service not implemented exception. Service : " + provider);
+
+            var ctors = service.GetConstructors().FirstOrDefault();
+            var parameters = ctors.GetParameters();
+            List<object> ctorServices = new List<object>();
+            foreach (var parameter in parameters)
+            {
+                var findedService = serviceProvider.GetService(parameter.ParameterType);
+                ctorServices.Add(findedService);
+            }
+
+            var activator = (IIdentifier)Activator.CreateInstance(service, args: ctorServices.ToArray());
+            return activator;
+        }
+    }
+}
