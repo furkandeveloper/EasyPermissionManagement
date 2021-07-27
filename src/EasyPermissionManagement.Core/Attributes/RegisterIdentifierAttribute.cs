ce11653d62fe536ed1421e3225dfd7de3a34ea9e
furkandeveloper <furkan.dvlp@gmail.com>
2021-07-27 23:37:38 +0300

    ⛏ Add Register Identifier Attribute

---
+++ src/EasyPermissionManagement.Core/Attributes/RegisterIdentifierAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyPermissionManagement.Core.Attributes
+{
+    /// <summary>
+    /// Registeriation Attribute for Identifier objects
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
+    public class RegisterIdentifierAttribute : Attribute
+    {
+        public RegisterIdentifierAttribute(string provider)
+        {
+            Provider = provider;
+        }
+
+        public string Provider { get; }
+    }
+}
