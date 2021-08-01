b2b2ecd3169735ab4b5ed32441821f68b0b38f81
Furkan Güngör <furkan.dvlp@gmail.com>
2021-08-02 00:30:40 +0300

    ⛏ Add Service Collection Extension
    This extensions will uses initalize DI container for EasyPermissionManagement.Core

---
+++ src/EasyPermissionManagement.Core/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,20 @@
+﻿using EasyPermissionManagement.Core.Abstractions;
+using EasyPermissionManagement.Core.Concrete;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyPermissionManagement.Core.Extensions
+{
+    public static class ServiceCollectionExtension
+    {
+        public static IServiceCollection AddEasyPermission(this IServiceCollection services)
+        {
+            services.AddTransient<IPermissionDefinationContext, PermissionDefinationContext>();
+            return services;
+        }
+    }
+}
