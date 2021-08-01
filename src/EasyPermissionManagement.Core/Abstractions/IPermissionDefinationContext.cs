c5406976d1bd4c3b7f202263d706489947aca320
Furkan Güngör <furkan.dvlp@gmail.com>
2021-08-02 00:36:32 +0300

    ⛏ Implement Permission Defination Context
    - Add IPermissionDefinationContext interface
    - Add PermissionDefinationContext implementation class

---
+++ src/EasyPermissionManagement.Core/Abstractions/IPermissionDefinationContext.cs
@@ -0,0 +1,16 @@
+﻿using EasyPermissionManagement.Core.Entities;
+using EasyPermissionManagement.Core.Extensions;
+using EasyPermissionManagement.Core.Statics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyPermissionManagement.Core.Abstractions
+{
+    public interface IPermissionDefinationContext
+    {
+        void DefinePermission(string key, string provider);
+    }
+}
---
+++ src/EasyPermissionManagement.Core/Concrete/PermissionDefinationContext.cs
@@ -0,0 +1,20 @@
+﻿using EasyPermissionManagement.Core.Abstractions;
+using EasyPermissionManagement.Core.Statics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyPermissionManagement.Core.Concrete
+{
+    public class PermissionDefinationContext : IPermissionDefinationContext
+    {
+        public void DefinePermission(string key, string provider)
+        {
+            var permission = new PermissionValue(key, provider);
+
+            PermissionValues.Permissions.Add(permission);
+        }
+    }
+}
