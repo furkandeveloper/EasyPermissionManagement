7bc53fedf9c5a98b4e7110f0d349d075bac1b86f
Furkan Güngör <furkan.dvlp@gmail.com>
2021-08-02 00:40:31 +0300

    ⛏ Create Permission Provider

---
+++ src/EasyPermissionManagement.Core/Abstractions/PermissionProvider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyPermissionManagement.Core.Abstractions
+{
+    public abstract class PermissionProvider
+    {
+        public virtual void Set(IPermissionDefinationContext context)
+        {
+
+        }
+    }
+}
