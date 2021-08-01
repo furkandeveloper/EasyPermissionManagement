9184dc66d8abf6e98eca3cde0eb69833061f1fb7
Furkan Güngör <furkan.dvlp@gmail.com>
2021-08-02 00:30:01 +0300

    ⛏ Add Permission Value

---
+++ src/EasyPermissionManagement.Core/Statics/PermissionValue.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyPermissionManagement.Core.Statics
+{
+    public class PermissionValue
+    {
+        public PermissionValue(string key, string provider)
+        {
+            Key = key;
+            Provider = provider;
+        }
+
+        public string Key { get; }
+
+        public string Provider { get; }
+    }
+}
