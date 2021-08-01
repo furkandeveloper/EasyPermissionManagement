11daae5a555a768366ebea71d86547c01fa257d9
Furkan Güngör <furkan.dvlp@gmail.com>
2021-08-02 00:40:09 +0300

    ⛏ Add Sample Permission provider

---
+++ sample/EasyPermissionManagement.Web/Permissions/MyPermissionProvider.cs
@@ -0,0 +1,17 @@
+﻿using EasyPermissionManagement.Core.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EasyPermissionManagement.Web.Permissions
+{
+    public class MyPermissionProvider : PermissionProvider
+    {
+        public override void Set(IPermissionDefinationContext context)
+        {
+            context.DefinePermission(key: "lead.create", provider: "User");
+            base.Set(context);
+        }
+    }
+}
