90487f6f0ff4d71e162bad6515ee3b3a70f38c2a
Furkan Güngör <furkan.dvlp@gmail.com>
2021-08-02 00:29:43 +0300

    ⛏ Add Permission Values static class
    This class will uses when appliacation start sync database for permissions

---
+++ src/EasyPermissionManagement.Core/Statics/PermissionValues.cs
@@ -0,0 +1,14 @@
+﻿using EasyPermissionManagement.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyPermissionManagement.Core.Statics
+{
+    public static class PermissionValues
+    {
+        public static List<PermissionValue> Permissions { get; } = new List<PermissionValue>();
+    }
+}
