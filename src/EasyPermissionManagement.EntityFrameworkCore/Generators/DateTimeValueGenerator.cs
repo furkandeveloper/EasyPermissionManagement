39ec4865a1c98b887bac2cc8fb4e2def422d27a6
Furkan Güngör <furkan.dvlp@gmail.com>
2021-08-13 23:37:40 +0300

    ⛏ Add date time value generator

---
+++ src/EasyPermissionManagement.EntityFrameworkCore/Generators/DateTimeValueGenerator.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.ValueGeneration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyPermissionManagement.EntityFrameworkCore.Generators
+{
+    /// <summary>
+    /// Date Time Value Generator
+    /// </summary>
+    public class DateTimeValueGenerator : ValueGenerator
+    {
+        public override bool GeneratesTemporaryValues => false;
+
+        protected override object NextValue(EntityEntry entry)
+        {
+            return DateTime.UtcNow;
+        }
+    }
+}
