c4deebe5ff555f9d052c0ad8240688167c1a85a2
Furkan Güngör <furkan.dvlp@gmail.com>
2021-08-25 00:30:01 +0300

    🗃️ Add inital database migration script for postgresql implementation

---
+++ src/EasyPermissionManagement.PostgreSql/Migrations/20210824212925_Initial.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EasyPermissionManagement.PostgreSql.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Permissions",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Key = table.Column<string>(type: "text", nullable: false),
+                    Provider = table.Column<string>(type: "text", nullable: false),
+                    CreateDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    UpdateDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Permissions", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Permissions_CreateDate_UpdateDate_Key_Provider",
+                table: "Permissions",
+                columns: new[] { "CreateDate", "UpdateDate", "Key", "Provider" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Permissions");
+        }
+    }
+}
