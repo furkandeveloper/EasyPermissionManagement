75c20f41fa9965f79fc44e8791e02208d0e2d201
Furkan Güngör <furkan.dvlp@gmail.com>
2021-08-25 00:23:39 +0300

    ⛏ Create Design Time Db Context for EasyPermissionPostgreSql Dbcontext

---
+++ src/EasyPermissionManagement.PostgreSql/Context/DesignTimeDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyPermissionManagement.PostgreSql.Context
+{
+    public class DesignTimeDbContext : IDesignTimeDbContextFactory<EasyPermissionPostgreSqlDbContext>
+    {
+        public EasyPermissionPostgreSqlDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<EasyPermissionPostgreSqlDbContext>();
+            optionsBuilder.UseNpgsql("{YOUR_CONNECTION_STRING}");
+            return new EasyPermissionPostgreSqlDbContext(optionsBuilder.Options);
+        }
+    }
+}
