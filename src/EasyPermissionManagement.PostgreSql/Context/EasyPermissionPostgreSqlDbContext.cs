86af3cb340b0bcbc64308c0a27bfb77b61e4a4be
Furkan Güngör <furkan.dvlp@gmail.com>
2021-08-25 00:23:17 +0300

    ⛏ Create EasyPermissionPostgreSql Dbcontext

---
+++ src/EasyPermissionManagement.PostgreSql/Context/EasyPermissionPostgreSqlDbContext.cs
@@ -0,0 +1,24 @@
+﻿using EasyPermissionManagement.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyPermissionManagement.PostgreSql.Context
+{
+    /// <summary>
+    /// Easy Permission PostgreSql Db Context
+    /// </summary>
+    public class EasyPermissionPostgreSqlDbContext : EasyPermissionCoreDbContext
+    {
+        public EasyPermissionPostgreSqlDbContext()
+        {
+        }
+
+        public EasyPermissionPostgreSqlDbContext(DbContextOptions<EasyPermissionPostgreSqlDbContext> options) : base(options)
+        {
+        }
+    }
+}
