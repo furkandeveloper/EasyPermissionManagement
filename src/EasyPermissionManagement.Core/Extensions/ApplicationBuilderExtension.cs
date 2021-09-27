7883acd79dfb8f93bba6b5c3bd0f670dc510c476
Furkan Güngör <furkan.dvlp@gmail.com>
2021-09-28 00:10:52 +0300

    ⛏ Implement initalize permission scenario

--- src/EasyPermissionManagement.Core/Extensions/ApplicationBuilderExtension.cs
+++ src/EasyPermissionManagement.Core/Extensions/ApplicationBuilderExtension.cs
@@ -7,6 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using EasyPermissionManagement.Core.Statics;
+using EasyPermissionManagement.Core.Entities;
 
 namespace EasyPermissionManagement.Core.Extensions
 {
@@ -28,8 +29,37 @@ namespace EasyPermissionManagement.Core.Extensions
             }
             if (PermissionValues.Permissions.Count > 0)
             {
-                // Insert or Update permissions
+                // Insert or Delete permissions
                 var context = app.ApplicationServices.GetRequiredService<IEasyPermissionContext>();
+                var permissionList = context.Get<Permission>().ToList();
+                foreach (var permission in PermissionValues.Permissions)
+                {
+                    var findedPermission = permissionList.FirstOrDefault(a => a.Key == permission.Key && a.Provider == permission.Provider);
+                    if (findedPermission is null)
+                    {
+                        // Insert
+                        context.Insert<Permission>(new Permission { Provider = permission.Provider, Key = permission.Key });
+                    }
+                }
+                var permissionSelectedList = permissionList.Select(s => new
+                {
+                    Key = s.Key,
+                    Provider = s.Provider
+                }).ToList();
+
+                var definedSelectedList = PermissionValues.Permissions.Select(s => new
+                {
+                    Key = s.Key,
+                    Provider = s.Provider
+                }).ToList();
+
+                var deletedPermissions = permissionSelectedList.Except(definedSelectedList).ToList();
+
+                foreach (var permission in deletedPermissions)
+                {
+                    // Delete
+                    context.Delete<Permission>(permissionList.FirstOrDefault(a=>a.Key == permission.Key && a.Provider == permission.Provider));
+                }
             }
         }
     }
