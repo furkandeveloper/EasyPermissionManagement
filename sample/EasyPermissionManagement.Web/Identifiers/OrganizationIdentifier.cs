8b131c7b4689dfe7672c99e54007a7a92f3d02f1
Furkan Güngör <furkan.dvlp@gmail.com>
2021-09-28 00:07:01 +0300

    ⛏ Add sample organization identifier

---
+++ sample/EasyPermissionManagement.Web/Identifiers/OrganizationIdentifier.cs
@@ -0,0 +1,19 @@
+﻿using EasyPermissionManagement.Core.Abstractions;
+using EasyPermissionManagement.Core.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EasyPermissionManagement.Web.Identifiers
+{
+    [RegisterIdentifier(PROVIDER)]
+    public class OrganizationIdentifier : IIdentifier
+    {
+        public const string PROVIDER = "Organization";
+        public Task<string> GetAsync(string provider)
+        {
+            return Task.FromResult("1");
+        }
+    }
+}
