c13fc23c4038f4b6ac4507f2514ddc009990dbf5
Furkan Güngör <furkan.dvlp@gmail.com>
2021-10-10 15:08:07 +0300

    ⛏ Implement Permission Checker Manager

---
+++ src/EasyPermissionManagement.Core/Concrete/PermissionCheckerManager.cs
@@ -0,0 +1,32 @@
+﻿using EasyPermissionManagement.Core.Abstractions;
+using EasyPermissionManagement.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyPermissionManagement.Core.Concrete
+{
+    public class PermissionCheckerManager : IPermissionChecker
+    {
+        private readonly IEasyPermissionContext context;
+
+        public PermissionCheckerManager(IEasyPermissionContext context)
+        {
+            this.context = context;
+        }
+        public virtual Task<bool> CheckAsync(string permissionKey, string provider, string identifierKey)
+        {
+            var permission = context.Get<Permission>().FirstOrDefault(a => a.Key == permissionKey && a.Provider == provider);
+            
+            if (permission is null)
+            {
+                throw new Exception($"Permission {permissionKey} not found");
+            }
+
+            return Task.FromResult(context.Get<IdentifierPermission>()
+                .Any(a => a.PermissionId == permission.Id && a.IdentifierKey == identifierKey));
+        }
+    }
+}
