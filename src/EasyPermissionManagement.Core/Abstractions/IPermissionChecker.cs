29706c69f22c2c7bb9bf3b0bcdaa43cffba97e64
Furkan Güngör <furkan.dvlp@gmail.com>
2021-10-10 15:03:26 +0300

    ⛏ Add Permission Checker helper service for interface layer

---
+++ src/EasyPermissionManagement.Core/Abstractions/IPermissionChecker.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyPermissionManagement.Core.Abstractions
+{
+    public interface IPermissionChecker
+    {
+        Task<bool> CheckAsync(string permissionKey, string provider, string identifierKey);
+    }
+}
