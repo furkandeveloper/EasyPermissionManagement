5d245a32bcdc5d589a409c0e87c813e559c3e4a0
Furkan Güngör <furkan.dvlp@gmail.com>
2021-10-10 23:26:46 +0300

    ⛏ Implement swagger ui

--- sample/EasyPermissionManagement.Web/Startup.cs
+++ sample/EasyPermissionManagement.Web/Startup.cs
@@ -6,6 +6,9 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System;
+using System.IO;
+using System.Reflection;
 
 namespace EasyPermissionManagement.Web
 {
@@ -24,6 +27,29 @@ namespace EasyPermissionManagement.Web
             services.AddControllers();
             services.AddEasyPermission();
             services.UseEasyPermissionNpgsql(options => options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("EasyPermissionManagement", new Microsoft.OpenApi.Models.OpenApiInfo()
+                {
+                    Title = "Easy Permission Management",
+                    Version = "1.0.0",
+                    Description = "This repo, provides query profiler for EF Core.",
+                    Contact = new Microsoft.OpenApi.Models.OpenApiContact()
+                    {
+                        Email = "furkan.dvlp@gmail.com",
+                        Url = new Uri("https://github.com/furkandeveloper/EasyProfiler")
+                    }
+                });
+                var docFile = $"{Assembly.GetEntryAssembly().GetName().Name}.xml";
+                var filePath = Path.Combine(AppContext.BaseDirectory, docFile);
+
+                if (File.Exists((filePath)))
+                {
+                    options.IncludeXmlComments(filePath);
+                }
+                options.DescribeAllParametersInCamelCase();
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,6 +65,18 @@ namespace EasyPermissionManagement.Web
             app.ApplyEasyPermission();
 
             app.UseRouting();
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(options =>
+            {
+                options.EnableDeepLinking();
+                options.ShowExtensions();
+                options.DisplayRequestDuration();
+                options.DocExpansion(Swashbuckle.AspNetCore.SwaggerUI.DocExpansion.None);
+                options.RoutePrefix = "api-docs";
+                options.SwaggerEndpoint("/swagger/EasyPermissionManagement/swagger.json", "EasyPermissionManagementSwagger");
+            });
 
             app.UseEndpoints(endpoints =>
             {
