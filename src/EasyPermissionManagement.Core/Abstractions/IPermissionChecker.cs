c1a3120712dc72961519f5a1e0d3e7f8498dd844
Furkan Güngör <furkan.dvlp@gmail.com>
2021-10-12 23:23:27 +0300

    📝 Add xml comment for IPermissionChecker

--- src/EasyPermissionManagement.Core/Abstractions/IPermissionChecker.cs
+++ src/EasyPermissionManagement.Core/Abstractions/IPermissionChecker.cs
@@ -6,8 +6,26 @@ using System.Threading.Tasks;
 
 namespace EasyPermissionManagement.Core.Abstractions
 {
+    /// <summary>
+    /// Permission Checker
+    /// </summary>
     public interface IPermissionChecker
     {
+        /// <summary>
+        /// Check Permission
+        /// </summary>
+        /// <param name="permissionKey">
+        /// Key of Permission
+        /// </param>
+        /// <param name="provider">
+        /// Identifier Method
+        /// </param>
+        /// <param name="identifierKey">
+        /// Identifier Key
+        /// </param>
+        /// <returns>
+        /// Task of bool
+        /// </returns>
         Task<bool> CheckAsync(string permissionKey, string provider, string identifierKey);
     }
 }
