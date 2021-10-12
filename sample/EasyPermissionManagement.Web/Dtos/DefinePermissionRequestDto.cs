6c63469edc9e536b73881577c2d1d53f9d51639e
Furkan Güngör <furkan.dvlp@gmail.com>
2021-10-12 23:17:41 +0300

    📝 Add xml comment for Define Permission request data transfer object

---
+++ sample/EasyPermissionManagement.Web/Dtos/DefinePermissionRequestDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EasyPermissionManagement.Web.Dtos
+{
+    /// <summary>
+    /// Permission Request Data Transfer Object
+    /// </summary>
+    public class DefinePermissionRequestDto
+    {
+        /// <summary>
+        /// Key of Permission
+        /// </summary>
+        public string PermissionKey { get; set; }
+
+        /// <summary>
+        /// Identifier Provider For Example; User, Organization etc.
+        /// </summary>
+        public string Provider { get; set; }
+
+        /// <summary>
+        /// Identifier Key
+        /// </summary>
+        public string IdentifierKey { get; set; }
+    }
+}
