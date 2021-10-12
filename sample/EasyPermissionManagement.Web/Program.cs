5490526d3ca79c14bf9e8d79eb4bf6781493dd33
Furkan Güngör <furkan.dvlp@gmail.com>
2021-10-12 23:21:04 +0300

    📝 Add xml comment for Program.cs

--- sample/EasyPermissionManagement.Web/Program.cs
+++ sample/EasyPermissionManagement.Web/Program.cs
@@ -9,13 +9,22 @@ using System.Threading.Tasks;
 
 namespace EasyPermissionManagement.Web
 {
+    /// <summary>
+    /// Program.cs
+    /// </summary>
     public class Program
     {
+        /// <summary>
+        /// Main Method
+        /// </summary>
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
         }
 
+        /// <summary>
+        /// Create Host
+        /// </summary>
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
