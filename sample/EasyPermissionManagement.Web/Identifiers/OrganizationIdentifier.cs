25d941b8f86d763668f15a7bae80e094e50eafb0
Furkan Güngör <furkan.dvlp@gmail.com>
2021-10-12 23:18:51 +0300

    ⛏ Implement Identifier methods

--- sample/EasyPermissionManagement.Web/Identifiers/OrganizationIdentifier.cs
+++ sample/EasyPermissionManagement.Web/Identifiers/OrganizationIdentifier.cs
@@ -7,13 +7,38 @@ using System.Threading.Tasks;
 
 namespace EasyPermissionManagement.Web.Identifiers
 {
+    /// <summary>
+    /// Organization Identifier
+    /// </summary>
     [RegisterIdentifier(PROVIDER)]
     public class OrganizationIdentifier : IIdentifier
-    {
+    {        
+        /// <summary>
+        /// Const Provider
+        /// </summary>
         public const string PROVIDER = "Organization";
-        public Task<string> GetAsync(string provider)
+
+        /// <summary>
+        /// Get Identifier Key
+        /// </summary>
+        /// <returns>
+        /// Task of String
+        /// </returns>
+        public Task<string> GetAsync()
         {
-            return Task.FromResult("1");
+            return Task.FromResult(GetFakeOrganizationName());
+        }
+
+        /// <summary>
+        /// Fake Organization Finder
+        /// </summary>
+        /// <returns>
+        /// string
+        /// </returns>
+        private string GetFakeOrganizationName()
+        {
+            // From Database
+            return "Organization 1";
         }
     }
 }
--- sample/EasyPermissionManagement.Web/Identifiers/UserIdentifier.cs
+++ sample/EasyPermissionManagement.Web/Identifiers/UserIdentifier.cs
@@ -1,5 +1,6 @@
 ﻿using EasyPermissionManagement.Core.Abstractions;
 using EasyPermissionManagement.Core.Attributes;
+using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,13 +8,40 @@ using System.Threading.Tasks;
 
 namespace EasyPermissionManagement.Web.Identifiers
 {
+    /// <summary>
+    /// User Identifier Method
+    /// </summary>
     [RegisterIdentifier(PROVIDER)]
     public class UserIdentifier : IIdentifier
     {
-        public const string PROVIDER = "User";
-        public Task<string> GetAsync(string provider)
+        /// <summary>
+        /// Ctor
+        /// </summary>
+        /// <param name="httpContextAccessor">
+        /// Provides access to the current Microsoft.AspNetCore.Http.IHttpContextAccessor.HttpContext,
+        /// if one is available.
+        /// </param>
+        public UserIdentifier(IHttpContextAccessor httpContextAccessor)
         {
-            return Task.FromResult("4");
+            this.httpContextAccessor = httpContextAccessor;
+        }
+
+        /// <summary>
+        /// Provider name
+        /// </summary>
+        public const string PROVIDER = "User";
+        private readonly IHttpContextAccessor httpContextAccessor;
+
+        /// <summary>
+        /// Find Identifier Key
+        /// </summary>
+        /// <returns>
+        /// Task of String
+        /// </returns>
+        public Task<string> GetAsync()
+        {
+            string id = httpContextAccessor.HttpContext.Request.RouteValues["id"]?.ToString();
+            return Task.FromResult(id);
         }
     }
 }
