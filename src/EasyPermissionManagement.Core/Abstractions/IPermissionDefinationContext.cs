738315066afc91015dff7b71b415c9eb56a50e5b
Furkan Güngör <furkan.dvlp@gmail.com>
2021-10-13 17:04:15 +0300

    📝 Add xml code comment for Permission Defination Context

--- src/EasyPermissionManagement.Core/Abstractions/IPermissionDefinationContext.cs
+++ src/EasyPermissionManagement.Core/Abstractions/IPermissionDefinationContext.cs
@@ -1,7 +1,19 @@
 ﻿namespace EasyPermissionManagement.Core.Abstractions
 {
+    /// <summary>
+    /// Permission Defination Context
+    /// </summary>
     public interface IPermissionDefinationContext
     {
-        void DefinePermission(string key, string provider, bool isDefault = false);
+        /// <summary>
+        /// Define Permission
+        /// </summary>
+        /// <param name="key">
+        /// Key of Permission
+        /// </param>
+        /// <param name="provider">
+        /// Identifier Method
+        /// </param>
+        void DefinePermission(string key, string provider);
     }
 }
