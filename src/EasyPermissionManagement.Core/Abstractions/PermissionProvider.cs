271904a16011f3add325bdf1fe5517750c1b87ff
Furkan Güngör <furkan.dvlp@gmail.com>
2021-10-13 17:04:33 +0300

    📝 Add xml code comment for Permission Provider

--- src/EasyPermissionManagement.Core/Abstractions/PermissionProvider.cs
+++ src/EasyPermissionManagement.Core/Abstractions/PermissionProvider.cs
@@ -6,8 +6,17 @@ using System.Threading.Tasks;
 
 namespace EasyPermissionManagement.Core.Abstractions
 {
+    /// <summary>
+    /// Permission Provider
+    /// </summary>
     public abstract class PermissionProvider
     {
+        /// <summary>
+        /// Set Permission
+        /// </summary>
+        /// <param name="context">
+        /// Permission Defination Context
+        /// </param>
         public virtual void Set(IPermissionDefinationContext context)
         {
 
