fcc6655ea10ff77ebfa5c47e09d3b87def54e881
Furkan Güngör <furkan.dvlp@gmail.com>
2021-10-14 23:06:29 +0300

    📝 Add xml code comment for Easy Permission Core Db Context

--- src/EasyPermissionManagement.EntityFrameworkCore/EasyPermissionCoreDbContext.cs
+++ src/EasyPermissionManagement.EntityFrameworkCore/EasyPermissionCoreDbContext.cs
@@ -16,21 +16,48 @@ namespace EasyPermissionManagement.EntityFrameworkCore
     /// </summary>
     public abstract class EasyPermissionCoreDbContext : DbContext, IEasyPermissionContext
     {
+        /// <summary>
+        /// Protected Ctor
+        /// </summary>
         protected EasyPermissionCoreDbContext()
         {
         }
 
+        /// <summary>
+        /// Protected ctor
+        /// </summary>
+        /// <param name="options">
+        /// The options to be used by a Microsoft.EntityFrameworkCore.DbContext. You normally override Microsoft.EntityFrameworkCore.DbContext.OnConfiguring(Microsoft.EntityFrameworkCore.DbContextOptionsBuilder) or use a Microsoft.EntityFrameworkCore.DbContextOptionsBuilder to create instances of this class and it is not designed to be directly constructed in your application code. 
+        /// </param>
         protected EasyPermissionCoreDbContext(DbContextOptions options) : base(options)
         {
         }
 
         #region Tables
+        /// <summary>
+        /// Permission Entity
+        /// </summary>
         public virtual DbSet<Permission> Permissions { get; set; }
 
+        /// <summary>
+        /// Identifier Permission
+        /// </summary>
         public virtual DbSet<IdentifierPermission> IdentifierPermissions { get; set; }
         #endregion
 
-
+        /// <summary>
+        /// On Model Creating
+        /// </summary>
+        /// <param name="modelBuilder">
+        /// Provides a simple API surface for configuring a Microsoft.EntityFrameworkCore.Metadata.IMutableModel
+        /// that defines the shape of your entities, the relationships between them, and
+        /// how they map to the database.
+        /// You can use Microsoft.EntityFrameworkCore.ModelBuilder to construct a model for
+        /// a context by overriding Microsoft.EntityFrameworkCore.DbContext.OnModelCreating(Microsoft.EntityFrameworkCore.ModelBuilder)
+        /// on your derived context. Alternatively you can create the model externally and
+        /// set it on a Microsoft.EntityFrameworkCore.DbContextOptions instance that is passed
+        /// to the context constructor.
+        /// </param>
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
@@ -112,11 +139,32 @@ namespace EasyPermissionManagement.EntityFrameworkCore
             });
         }
 
+        /// <summary>
+        /// Get Queryable Entity
+        /// </summary>
+        /// <typeparam name="T">
+        /// Type of Entity
+        /// </typeparam>
+        /// <returns>
+        /// Type of IQueryable
+        /// </returns>
         public IQueryable<T> Get<T>() where T : class
         {
             return Set<T>().AsQueryable();
         }
 
+        /// <summary>
+        /// Insert Entity
+        /// </summary>
+        /// <typeparam name="T">
+        /// Type of Entity
+        /// </typeparam>
+        /// <param name="entity">
+        /// Entity
+        /// </param>
+        /// <returns>
+        /// Task
+        /// </returns>
         public virtual async Task<T> InsertAsync<T>(T entity) where T : class
         {
             var entry = Entry(entity);
@@ -125,6 +173,18 @@ namespace EasyPermissionManagement.EntityFrameworkCore
             return entry.Entity;
         }
 
+        /// <summary>
+        /// Update Entity
+        /// </summary>
+        /// <typeparam name="T">
+        /// Type of Entity
+        /// </typeparam>
+        /// <param name="entity">
+        /// Entity
+        /// </param>
+        /// <returns>
+        /// Task
+        /// </returns>
         public virtual async Task<T> UpdateAsync<T>(T entity) where T : class
         {
             var entry = Entry(entity);
@@ -133,12 +193,36 @@ namespace EasyPermissionManagement.EntityFrameworkCore
             return entry.Entity;
         }
 
+        /// <summary>
+        /// Delete
+        /// </summary>
+        /// <typeparam name="T">
+        /// Type of Entity
+        /// </typeparam>
+        /// <param name="entity">
+        /// Entity
+        /// </param>
+        /// <returns>
+        /// Task
+        /// </returns>
         public virtual async Task DeleteAsync<T>(T entity) where T : class
         {
             Set<T>().Remove(entity);
             await SaveChangesAsync();
         }
 
+        /// <summary>
+        /// Insert Entity
+        /// </summary>
+        /// <typeparam name="T">
+        /// Type of Entity
+        /// </typeparam>
+        /// <param name="entity">
+        /// Entity
+        /// </param>
+        /// <returns>
+        /// Entity
+        /// </returns>
         public virtual T Insert<T>(T entity) where T : class
         {
             var entry = Entry(entity);
@@ -147,6 +231,18 @@ namespace EasyPermissionManagement.EntityFrameworkCore
             return entry.Entity;
         }
 
+        /// <summary>
+        /// Update Entity
+        /// </summary>
+        /// <typeparam name="T">
+        /// Type of Entity
+        /// </typeparam>
+        /// <param name="entity">
+        /// Entity
+        /// </param>
+        /// <returns>
+        /// Entity
+        /// </returns>
         public virtual T Replace<T>(T entity) where T : class
         {
             var entry = Entry(entity);
@@ -155,6 +251,15 @@ namespace EasyPermissionManagement.EntityFrameworkCore
             return entry.Entity;
         }
 
+        /// <summary>
+        /// Delete Entity
+        /// </summary>
+        /// <typeparam name="T">
+        /// Type of Entity
+        /// </typeparam>
+        /// <param name="entity">
+        /// Entity
+        /// </param>
         public virtual void Delete<T>(T entity) where T : class
         {
             Set<T>().Remove(entity);
