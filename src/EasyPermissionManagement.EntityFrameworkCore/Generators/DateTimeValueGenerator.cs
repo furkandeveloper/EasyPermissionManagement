5e4bc31cde563ef07e6aee5b5457d37299c36a80
Furkan Güngör <furkan.dvlp@gmail.com>
2021-10-14 23:06:00 +0300

    📝 Add xml code comment for Date Time Value Generator

--- src/EasyPermissionManagement.EntityFrameworkCore/Generators/DateTimeValueGenerator.cs
+++ src/EasyPermissionManagement.EntityFrameworkCore/Generators/DateTimeValueGenerator.cs
@@ -13,8 +13,21 @@ namespace EasyPermissionManagement.EntityFrameworkCore.Generators
     /// </summary>
     public class DateTimeValueGenerator : ValueGenerator
     {
+        /// <summary>
+        /// GeneratesTemporaryValues
+        /// </summary>
         public override bool GeneratesTemporaryValues => false;
 
+        /// <summary>
+        /// Generate value
+        /// </summary>
+        /// <param name="entry">
+        /// Provides access to change tracking information and operations for a given entity.
+        /// Instances of this class are returned from methods when using the Microsoft.EntityFrameworkCore.ChangeTracking.ChangeTracker</param>
+        /// API and it is not designed to be directly constructed in your application code.
+        /// <returns>
+        /// Object
+        /// </returns>
         protected override object NextValue(EntityEntry entry)
         {
             return DateTime.UtcNow;
